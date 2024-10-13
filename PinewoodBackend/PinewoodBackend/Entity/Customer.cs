97f93d32210529d613d81fa97596fbc3d1faffc8
sxg661 <sophie.guile@yahoo.co.uk>
2024-10-13 02:14:35 +0100

    Add temporary customer entity

---
+++ PinewoodBackend/PinewoodBackend/Entity/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace PinewoodBackend.Entity
+{
+	public class Customer
+	{
+		public static List<Customer> customers = [];
+		public static string NewId => Guid.NewGuid().ToString();
+		public string Id { get; set; }
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public DateTime DateOfBirth { get; set; }
+	}
+}
